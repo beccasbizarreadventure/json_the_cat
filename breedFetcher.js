70b8c05de9e924d040ba6f16f7c85a924bc58d00
Rebecca <rebecca90469@gmail.com>
2024-02-20 14:25:26 -0500

    Seperated fuction from the rest of the logic for this problem into index.js, keeping the main function in the original file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
 
-const fullBreedName = process.argv[2];
-const breed = fullBreedName.slice(0, 4);
-let URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = function(breedName, callback) {
+  let search = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(search, function(error, response, body) {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+      console.error(error);
+      return;
+    }
+    if (data[0] === undefined) {
+      throw new Error("Looks like that cat's gone missing");
+    }
+    callback(null, data[0].description);
+  });
+};
 
-request(URL, function(error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    throw new Error("Looks like that cat's gone missing");
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
